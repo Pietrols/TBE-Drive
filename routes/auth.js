c9e75b9463a3c3c9c184a0815048ca57ebaccfa1
Peter Kabamba <kabambapeter3@gmail.com>
2026-02-26 00:04:43 +0200

    add login and logout api routes to auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,24 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const registerValidator = require("../middleware/validators");
+const passport = require("passport");
 
-// get register page
+// registration routes
 router.get("/register", authController.showRegisterForm);
-
-// post register page
 router.post("/register", registerValidator, authController.registerUser);
+
+// login routes
+router.get("/login", authController.showLoginForm);
+router.post(
+  "login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+);
+
+// logout route
+router.post("/logout", authController.logoutUser);
 
 module.exports = router;
